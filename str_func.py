b272a4cf63b79e5097b1058df819a64cd5c92442
Upycuk <iraida.com@yandex.ru>
2024-05-11 07:25:40 +0300

    Commit capitalize line

--- str_func.py
+++ str_func.py
@@ -1,3 +1,14 @@
 def function() -> str:
-    line = input().upper()
-    return print(line)
+    """
+    Функция, которая делает заглавными первые буквы каждого слова в строке
+    :return: str
+    """
+    return print(input().upper())
+
+
+def function2() -> str:
+    """
+    Функция, которая делает заглавными первые буквы каждого слова в строке
+    :return: str
+    """
+    return print(input().capitalize())
